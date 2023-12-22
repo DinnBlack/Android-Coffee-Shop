170dc4458e44ab20571eb7dd0d32edd9a0ea07f6
Dinh Hoang Phuc <52100087@student.tdtu.edu.vn>
2023-12-23 02:22:51 +0700

    Committing changes to build.gradle.kts

---
+++ TheFutusCoffeeVersion13/app/src/main/java/com/example/thefutuscoffeeversion13/Activity/ManageEmployeeActivity.java
@@ -0,0 +1,16 @@
+package com.example.thefutuscoffeeversion13.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.thefutuscoffeeversion13.R;
+
+public class ManageEmployeeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_employee);
+    }
+}
